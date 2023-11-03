e3f331294e06f19b62654d9f987d25e1e9abffb0
xonx4l <sk797119@gmail.com>
2023-11-03 19:37:56 +0530

    page.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  
+  return (
+     <div className="bg-gradient-to-r min-h-screen from rose-100 to teal-10">
+        
+     </div>
+  )
 }
