05d13c039e561679402c5ada8a7cd73292c3bffd
xonx4l <sk797119@gmail.com>
2023-11-04 10:57:37 +0530

    page.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-     <div className="bg-gradient-to-r min-h-screen from rose-100 to teal-10">
-        
+     <div className="bg-gradient-to-r min-h-screen grainy from rose-100 to teal-10">
+       <div className= "absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2">
+       <h1 className="font-semibold text-7xl text-center"></h1>
+       </div>    
      </div>
   )
 }
