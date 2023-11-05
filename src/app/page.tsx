5ac113a9b347a6707e1c4fd757d3f2eec0914882
xonx4l <sk797119@gmail.com>
2023-11-05 16:22:09 +0530

    page.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
      <div className="bg-gradient-to-r min-h-screen grainy from rose-100 to teal-10">
        <div className= "absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2">
-       <h1 className="font-semibold text-7xl text-center"></h1>
+       <h1 className="font-semibold text-7xl text-center">
+        AI <span className="text-green-600 font-bold"> note taking </span> assistant.
+       </h1>
+       <div className="mt-4"></div>
+       <h2 className="font-semibold text-3xl text-center text-slate-700 ">
+        AI Powered 
+       </h2>
        </div>    
      </div>
   )
