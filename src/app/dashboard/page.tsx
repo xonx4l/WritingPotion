af95f82eaf983aa5b5ea58c26ac84d5e8e6e9a28
xonx4l <sk797119@gmail.com>
2023-11-08 15:50:52 +0530

    add back button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from 'react'
 
 type Props = {};
 
 const DashboardPage = (props: Props) => {
-    return <div>DashboardPage</div>;
+    return(
+        <>
+        <div className="grainy min-h-screen">
+          <div className="max-w-7xl mx-auto p-10"> 
+             <div className="h-14"></div>
+             <div className ="flex justify-between  items-center md:flex-row flex-col">
+                <div className= "flex items-center">
+                    <Link href='/'>
+                        <Button>Back</Button>
+                    </Link>
+                </div>
+             </div>
+          </div>  
+        </div>
+        </>
+    )
     
 };
 
