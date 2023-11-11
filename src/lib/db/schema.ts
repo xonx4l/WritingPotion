abe623d66556f94ff61334fe79b3292279b92e3e
xonx4l <sk797119@gmail.com>
2023-11-11 16:22:06 +0530

    update schema

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,10 @@
+import {pgTable,text,timestamp, serial} from 'drizzle-orm/pg-core'
+
+export const $notes =pgTable('notes',{
+     id: serial('id'). primaryKey(),
+     name: text('name').notNull(),
+     createdAt: timestamp('created_at').notNull().defaultNow(),
+     imageUrl: text('iamgeUrl'),
+     userId: text('user_id').notNull(),
+     editorState: text('editor_state'),
+});
