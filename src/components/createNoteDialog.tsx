c3515713f96b1827642458314eaa5892c0aea654
xonx4l <sk797119@gmail.com>
2023-11-18 16:22:43 +0530

    update dialog box

--- src/components/createNoteDialog.tsx
+++ src/components/createNoteDialog.tsx
@@ -1,10 +1,19 @@
 'use client'
 import React from 'react'
+import { Dialog, DialogTrigger } from './ui/dialog'
+import { Plus } from 'lucide-react'
 
 type Props ={}
 
 const CreateNoteDialog = (props: Props) => {
     return (
-        <div>CreateNoteDialog</div>
+       <Dialog>
+          <DialogTrigger>
+               <div className='border-dashed border-2 border-green-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-x1 transition hover:-translate-y-1 flex-row p-4'>
+                   <Plus className="w-6 h-6 text-green-600 " strokeWidth={3} />
+                   <h2 className="font-semibold text-green-600 sm:mt-2">New Note</h2>
+               </div>
+          </DialogTrigger>
+       </Dialog>
     )
 }
