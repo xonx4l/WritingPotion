2afa919e6c9a42005a280290ffdf492ac002ca15
xonx4l <sk797119@gmail.com>
2023-11-26 10:29:13 +0530

    update openai.ts

---
+++ src/lib/openai.ts
@@ -0,0 +1,9 @@
+export async function generateImagePrompt(prompt: string){
+      // math
+
+      // -> :thumbnail has a bunch of colourful numbers and text.
+}
+
+export async function generateImage() {
+
+}
